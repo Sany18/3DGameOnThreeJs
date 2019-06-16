6c78b2c443e515a961d5100d2bd5f5335ccee382
Hoxz <parubok.sashko@gmail.com>
2019-06-16 22:23:23 +0300

    Upgrade console

--- game/other.js
+++ game/other.js
@@ -11,21 +11,47 @@
 		customConsoleText.innerHTML += '<br>' + msg
 	}
 
-	addText('Custom console v0.0.1')
+	let commands = {
+		help: () => {
+			addText('<br>help:')
+			addText(' - show config: config')
+			addText(' - show help: help')
+			addText(' - clear console: clear')
+			addText('<br>set property:')
+			addText(' - gravity 9.8 <br>')
+		},
+		config: () => {
+			addText('<br>')
+			for (var key in window.config) {
+				addText(key + ': ' + window.config[key])
+			}
+		},
+		clear: () => {
+			customConsoleText.innerHTML = ''
+		},
+		setConfig: (value) => {
+			let arr = value.split(' ')
 
-	let help = function() {
-		for (var key in window.config) {
-			console.log(key)
+			if (arr[0] in window.config) {
+				window.config[arr[0]] = arr[1]
+				addText('assigned ' + value)
+				value = ''
+			} else {
+				addText('command not found')
+			}
 		}
 	}
 
-	!function () {
-		let log = console.log
-		console.log = function(msg) {
-			addText(msg)
-			log.call(this, ...arguments)
-		}
-	}()
+	addText('Custom console v0.0.2')
+	commands.help()
+
+	// !function () {
+	// 	let log = console.log
+	// 	console.log = function(msg) {
+	// 		addText(msg)
+	// 		log.call(this, ...arguments)
+	// 	}
+	// }()
 
 	document.addEventListener('keydown', function(e) {
 		if (e.keyCode == 192) {
@@ -42,12 +68,14 @@
 
 	customConsoleInput.addEventListener('keypress', function(e) {
 		if (e.keyCode == 13) {
-			switch(e.target.value) {
-				case 'help': help(); break;
-			}
-
 			e.preventDefault()
-			eval('window.config.' + e.target.value)
+
+			switch(e.target.value) {
+				case 'help': commands.help(); break;
+				case 'config': commands.config(); break;
+				case 'clear': commands.clear(); break;
+				default: commands.setConfig(e.target.value); break;
+			}
 		}
 	})
 }()
