baa2794ffd0dc0f151c97ab06797fe29ac2c86f4
Hoxz <parubok.sashko@gmail.com>
2019-06-16 15:38:50 +0300

    Added some params

--- game/config.js
+++ game/config.js
@@ -1,9 +1,10 @@
 window.config = {
-	debug: true,
+	debug: false,
+	enableShadows: true,
+	shadowResolution: 2048,
 	imagePrefix: `${location.origin}/game/textures/`,
 	moveSpeed: 400,
 	yourMass: 100.0,
 	gravity: 9.8,
-	jumpHeight: 40,
-	shadowResolution: 2048
+	jumpHeight: 40
 }
--- game/initWorld.js
+++ game/initWorld.js
@@ -43,12 +43,13 @@ let initWorld = function() {
     let backLight = new THREE.DirectionalLight(0xffffff, 0.3)
 
     light.position.set(50, 200, -200)
-    light.target.position.set(0, 0, 0)
-    light.castShadow = true
-    scene.add(light)
-
     backLight.position.set(-50, 200, 200)
-    scene.add(backLight)
+
+    light.target.position.set(0, 0, 0)
+
+    if (window.config.enableShadows) {
+      light.castShadow = true
+    }
 
     light.shadow.camera.left = -200
     light.shadow.camera.right = 200
@@ -57,8 +58,11 @@ let initWorld = function() {
     light.shadow.mapSize.width = window.config.shadowResolution
     light.shadow.mapSize.height = window.config.shadowResolution
 
+    scene.add(light)
+    scene.add(backLight)
+
     if (window.config.debug) {
-      let helper = new THREE.CameraHelper( light.shadow.camera ) // only for debugging
+      let helper = new THREE.CameraHelper(light.shadow.camera)
       scene.add(helper)
     }
 
@@ -66,24 +70,28 @@ let initWorld = function() {
   }()
 
   // stats
-  let stats = function() {
-    let stats = new Stats()
-    let stats2 = new Stats()
-    let stats3 = new Stats()
-    stats.showPanel(0)
-    stats2.showPanel(1)
-    stats3.showPanel(2)
-    stats.dom.style.cssText = 'position:absolute;bottom:0px;left:0px;'
-    stats2.dom.style.cssText = 'position:absolute;bottom:0px;left:80px;'
-    stats3.dom.style.cssText = 'position:absolute;bottom:0px;left:160px;'
-    document.body.appendChild(stats.dom)
-    document.body.appendChild(stats2.dom)
-    document.body.appendChild(stats3.dom)
+  let stats = { start: () => {}, end: () => {} }
 
-    function start() {stats.begin(); stats2.begin(); stats3.begin()}
-    function end() {stats.end(); stats2.end(); stats3.end()}
-    return {start, end}
-  }()
+  if (window.config.debug) {
+    stats = function() {
+      let stats = new Stats()
+      let stats2 = new Stats()
+      let stats3 = new Stats()
+      stats.showPanel(0)
+      stats2.showPanel(1)
+      stats3.showPanel(2)
+      stats.dom.style.cssText = 'position:absolute;bottom:0px;left:0px;'
+      stats2.dom.style.cssText = 'position:absolute;bottom:0px;left:80px;'
+      stats3.dom.style.cssText = 'position:absolute;bottom:0px;left:160px;'
+      document.body.appendChild(stats.dom)
+      document.body.appendChild(stats2.dom)
+      document.body.appendChild(stats3.dom)
+
+      function start() {stats.begin(); stats2.begin(); stats3.begin()}
+      function end() {stats.end(); stats2.end(); stats3.end()}
+      return {start, end}
+    }()
+  }
 
   //listeners
   !function listeners() {
